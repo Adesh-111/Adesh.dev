05f5ca608b4b8fdf4843a2320cd7dd6a71818989
Adesh-111 <adeshabinesh25@gmail.com>
2024-09-04 00:35:27 +0530

    90% completion of About section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -3,22 +3,26 @@ import assets from "../../assets/assets";
 function About() {
   return (
     <>
-      <div className="about" id="About">
-        <div className="about-image">
+      <div className="about-container">
+        <div className="about" id="About">
+          <div className="about-image">
             <img src={assets.aboutImg} alt="" />
-        </div>
+          </div>
 
           <div className="about-details">
-            <h3>Wait a minute, who am i?</h3>
+            <h3>
+              Wait a minute, <span>who am i?</span>
+            </h3>
             <p>
-              I am a Computer science and Engineering student at
-              Vel Tech High Tech, presently in my final year. I've been pursuing full-stack
+              I am a Computer science and Engineering student at Vel Tech High
+              Tech, presently in my final year. I've been pursuing full-stack
               web development since my second year and have a big interest in
               developing products, improving services and delivering value. My
               core ideals are of consistency, hard work, discipline, and grit.
             </p>
             <button>More about me</button>
           </div>
+        </div>
       </div>
     </>
   );
