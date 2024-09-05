030091dac76c454d33f2359b2466682add36e4ca
Adesh-111 <adeshabinesh25@gmail.com>
2024-09-05 22:26:58 +0530

    Completion of 75% of project section

--- src/components/Projects/Project.jsx
+++ src/components/Projects/Project.jsx
@@ -5,6 +5,7 @@ import "./Project.css"
 function Project({ project }) {
   return (
     <div className="project">
+       
       <div className="project-container">
         <div className="project-description">
           <div className="project-domain">
@@ -13,6 +14,7 @@ function Project({ project }) {
           </div>
           <h3>{project.title}</h3>
           <p className="description">{project.description}</p>
+          <button>{"More about " + project.title }<span>→</span></button>
         </div>
         <div className="project-image">
           <img src={project.image} alt={project.imageAlt || "Project Image"} />
@@ -25,7 +27,6 @@ function Project({ project }) {
 Project.propTypes = {
   project: PropTypes.shape({
     icon: PropTypes.string.isRequired,
-    buttonText: PropTypes.string.isRequired,
     description: PropTypes.string.isRequired,
     image: PropTypes.string.isRequired,
     imageAlt: PropTypes.string,
