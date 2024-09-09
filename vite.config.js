5c6ea2dcff1f7822626a0e7964fc9fafd3be1e04
Adesh-111 <adeshabinesh25@gmail.com>
2024-09-10 00:18:50 +0530

    updation of vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist', 
+    outDir: 'desc', // Change 'dist' to 'build' if you want Vercel to detect the folder as 'build'
   },
 })
