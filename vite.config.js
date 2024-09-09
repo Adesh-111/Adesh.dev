4e1eae6d644adb6b06aa148d9eac0385ef8b4f58
Adesh-111 <adeshabinesh25@gmail.com>
2024-09-10 00:27:04 +0530

    desc

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'build', // Ensure Vercel detects the output folder as 'build'
+    outDir: 'desc', // Ensure Vercel detects the output folder as 'build'
   },
   // Other Vite options...
 });
