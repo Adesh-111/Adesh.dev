ae623e9b7e5cdd408d2f6a634e614cd33be34f2c
Adesh-111 <adeshabinesh25@gmail.com>
2024-09-09 22:32:21 +0530

    dist for deployment

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Change 'dist' to 'build' if you want Vercel to detect the folder as 'build'
+  },
 })
