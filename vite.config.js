63edbbf10c34c07a239714b49f9b70a3bf695870
Adesh-111 <adeshabinesh25@gmail.com>
2024-09-10 00:33:31 +0530

    Final dist

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 // vite.config.js
 export default {
   build: {
-    outDir: 'desc', // Change 'dist' to 'build' if you want Vercel to detect the folder as 'build'
+    outDir: 'dist', // Change 'dist' to 'build' if you want Vercel to detect the folder as 'build'
   },
 };
