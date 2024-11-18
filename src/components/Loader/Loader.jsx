0a7d1235a18793052fcc2405244bfb59feaa262d
Adesh-111 <adeshabinesh25@gmail.com>
2024-11-18 10:45:34 +0530

    Add loader component

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,19 @@
+import { Triangle } from "react-loader-spinner";
+import "./loader.css"
+
+function Loader() {
+    return (
+        <div className="loader">
+        <Triangle
+            visible={true}
+            height="300"
+            width="300"
+            color="orange"
+            ariaLabel="triangle-loading"
+            wrapperStyle={{}}
+            wrapperClass=""
+        /></div>
+    );
+}
+
+export default Loader;
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,24 @@
-// src/main.jsx
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
-import router from "./components/AppRouter/AppRouter";// Assuming you set up your router in AppRouter.js or similar
+import router from "./components/AppRouter/AppRouter";
+import Loader from "./components/Loader/Loader";
+
+
+function Main() {
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        setTimeout(() => {
+            setLoading(false);
+        }, 3000); 
+    }, []);
+
+    return loading ? <Loader /> : <RouterProvider router={router} />;
+}
 
 createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+    <React.StrictMode>
+        <Main />
+    </React.StrictMode>
 );
